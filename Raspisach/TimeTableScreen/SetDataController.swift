835c895baca35ef77a06ac9a206710d03c3be468
 Evgeniy <spiralcka@gmail.com>
2019-06-06 20:21:34 +0300

    added tableview

--- Raspisach/TimeTableScreen/SetDataController.swift
+++ Raspisach/TimeTableScreen/SetDataController.swift
@@ -22,8 +22,25 @@ class SetDataController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    
 
+}
+
+extension SetDataController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SetDataCell
+        return cell
+        
+    }
+    
+    
 }
